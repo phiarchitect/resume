ef790b4f952a083f178e7d0213c3eb798853316b
phiarchitect <phi@phiarchitect.com>
2023-01-30 15:55:40 -0800

    update

--- generate.py
+++ generate.py
@@ -1,12 +1,12 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
-import argparse
+#  import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--type", required=False, help="type of resume")
-parser.add_argument("-o", "--outfile", required=True, help="output file name")
-parser.add_argument("-j", "--template", required=True, help="jinja template")
-args = parser.parse_args()
+#  parser = argparse.ArgumentParser()
+#  parser.add_argument("-t", "--type", required=False, help="type of resume")
+#  parser.add_argument("-o", "--outfile", required=True, help="output file name")
+#  parser.add_argument("-j", "--template", required=True, help="jinja template")
+#  args = parser.parse_args()
 
 with open("resume.yaml", "r") as fin:
     resume = yaml.safe_load(fin)
@@ -20,19 +20,24 @@ resume["style"] = style
 # This modification is to make sure the syntax
 # plays well with latex templates
 env = Environment(
-    block_start_string="\BLOCK{",
-    block_end_string="}",
-    variable_start_string="\VAR{",
-    variable_end_string="}",
-    comment_start_string="\#{",
-    comment_end_string="}",
-    line_statement_prefix="%%",
-    line_comment_prefix="%#",
+    #  block_start_string="\BLOCK{",
+    #  block_end_string="}",
+    #  variable_start_string="\VAR{",
+    #  variable_end_string="}",
+    #  comment_start_string="\#{",
+    #  comment_end_string="}",
+    #  line_statement_prefix="%%",
+    #  line_comment_prefix="%#",
     trim_blocks=True,
     autoescape=False,
-    loader=FileSystemLoader("./templates"),
+    loader=FileSystemLoader("./"),
 )
 
-template = env.get_template(args.template)
-with open(args.outfile, "w") as fout:
+templatefile = 'template.html'
+#  template = env.get_template(args.template)
+template = env.get_template(templatefile)
+#  outfile = args.outfile
+outfile = 'resume.html'
+
+with open(outfile, "w") as fout:
     fout.write(template.render(resume=resume))
